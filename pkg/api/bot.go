21a3a584503d17c306b3426ed2e51d80371ecbb6
Alim-Lyukmanov <arlyukmanov@mail.ru>
2025-02-23 08:20:56 +0300

    fix: Add Bot structure
    Add bot structure and structure-related functions to run telegram bot

--- pkg/api/bot.go
+++ pkg/api/bot.go
@@ -10,109 +10,118 @@ import (
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
 
-func StartBot() {
+type Bot struct {
+	Api     *tgbotapi.BotAPI
+	Updates tgbotapi.UpdatesChannel
+}
 
-	//получае токен бота из переменной окружения
-	botToken, exists := os.LookupEnv("BOT_API_KEY")
+func (bot *Bot) initBot(apiKeyEnvVariant string) {
+	//получаем токен бота из переменной окружения
+	token, exists := os.LookupEnv(apiKeyEnvVariant)
 	if !exists {
 		log.Fatal("BOT_API_KEY не найден")
 	}
 
 	//создаем новый экземляр бота
-	bot := initBot(botToken)
-
-	//устанавливаем режим отладки
-	bot.Debug = true
-
-	log.Printf("Аторизован как %s", bot.Self.UserName)
-
-	//канал для получения обновлений
-	u := initUpdatesConfig()
-
-	updates := bot.GetUpdatesChan(u)
-
-	//обработка входящих сообщений
-	processUpdates(bot, updates)
-}
-
-func initBot(token string) *tgbotapi.BotAPI {
-	bot, err := tgbotapi.NewBotAPI(token)
-
+	api, err := tgbotapi.NewBotAPI(token)
 	if err != nil {
-		log.Panic(err)
+		log.Fatal(err)
 	}
 
-	return bot
+	bot.Api = api
+	log.Printf("Аторизован как %s", bot.Api.Self.UserName)
+
+	//устанавливаем режим отладки
+	bot.Api.Debug = true
 }
 
-func initUpdatesConfig() tgbotapi.UpdateConfig {
+// канал для получения обновлений
+func (bot *Bot) initUpdatesChannel() {
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
-	return u
+	bot.Updates = bot.Api.GetUpdatesChan(u)
 }
 
-func processUpdates(bot *tgbotapi.BotAPI, updates tgbotapi.UpdatesChannel) {
-
-	for update := range updates {
-		if update.Message != nil { // проверка новго сообщения
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-
-			var msg tgbotapi.MessageConfig
-
-			switch update.Message.Text {
-			default:
-				{
-					msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Неизвестная команда")
-					msg.ReplyMarkup = buildReplyKeyboard()
-				}
-			case "Погода":
-				{
-					msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите город")
-					msg.ReplyMarkup = buildInlineKeyboardWeatherCity()
-				}
-			}
-			// отправка сообщения
-			if _, err := bot.Send(msg); err != nil {
-				log.Panic(err)
-			}
+// обработка входящих сообщений
+func (bot *Bot) loopThroughUpdates() {
+	for update := range bot.Updates {
+		if update.Message != nil {
+			bot.processMessage(update)
 		} else {
-			callbackData := update.CallbackQuery.Data
-			arr := strings.Split(callbackData, ":")
-			switch arr[0] {
-			case "weatherCity":
-				{
-					msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Выберите дату")
-					msg.ReplyMarkup = buildInlineKeyboardWeatherDate(callbackData)
-					// отправка сообщения
-					deleteMsg := tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID)
-					bot.Send(deleteMsg)
-					if _, err := bot.Send(msg); err != nil {
-						log.Panic(err)
-					}
-				}
-			case "weatherDate":
-				{
-					arr = strings.Split(callbackData, ";")
-					res := GetWeatherforecast(strings.Split(arr[1], ":")[1], strings.Split(arr[0], ":")[1])
-					msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, res)
-					msg.ReplyMarkup = nil
-					deleteMsg := tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID)
-					bot.Send(deleteMsg)
-					if _, err := bot.Send(msg); err != nil {
-						log.Panic(err)
-					}
-				}
-			default:
-				{
-					msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Неизвестная команда")
-					msg.ReplyMarkup = buildReplyKeyboard()
-					if _, err := bot.Send(msg); err != nil {
-						log.Panic(err)
-					}
-				}
+			if update.CallbackQuery != nil {
+				bot.processCallbackQuery(update)
 			}
 		}
 	}
+}
+
+func (bot *Bot) processMessage(update tgbotapi.Update) {
+	log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+	var msg tgbotapi.MessageConfig
+
+	switch update.Message.Text {
+	default:
+		{
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Неизвестная команда")
+			msg.ReplyMarkup = buildReplyKeyboard()
+		}
+	case "Погода":
+		{
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "Выберите город")
+			msg.ReplyMarkup = buildInlineKeyboardWeatherCity()
+		}
+	}
+	// отправка сообщения
+	if _, err := bot.Api.Send(msg); err != nil {
+		log.Panic(err)
+	}
+}
+
+func (bot *Bot) processCallbackQuery(update tgbotapi.Update) {
+	callbackData := update.CallbackQuery.Data
+	arr := strings.Split(callbackData, ":")
+	switch arr[0] {
+	case "weatherCity":
+		{
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Выберите дату")
+			msg.ReplyMarkup = buildInlineKeyboardWeatherDate(callbackData)
+			// отправка сообщения
+			deleteMsg := tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID)
+			bot.Api.Send(deleteMsg)
+			if _, err := bot.Api.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		}
+	case "weatherDate":
+		{
+			arr = strings.Split(callbackData, ";")
+			res := GetWeatherforecast(strings.Split(arr[1], ":")[1], strings.Split(arr[0], ":")[1])
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, res)
+			msg.ReplyMarkup = nil
+			deleteMsg := tgbotapi.NewDeleteMessage(update.CallbackQuery.Message.Chat.ID, update.CallbackQuery.Message.MessageID)
+			bot.Api.Send(deleteMsg)
+			if _, err := bot.Api.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		}
+	default:
+		{
+			msg := tgbotapi.NewMessage(update.CallbackQuery.Message.Chat.ID, "Неизвестная команда")
+			msg.ReplyMarkup = buildReplyKeyboard()
+			if _, err := bot.Api.Send(msg); err != nil {
+				log.Panic(err)
+			}
+		}
+	}
+}
+
+func RunBot() {
+
+	bot := new(Bot)
+	bot.initBot("BOT_API_KEY")
+	bot.initUpdatesChannel()
+	bot.loopThroughUpdates()
 }
 
 func buildReplyKeyboard() tgbotapi.ReplyKeyboardMarkup {
